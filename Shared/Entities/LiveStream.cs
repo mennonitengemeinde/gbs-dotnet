f82c827888a87d39445e8d354c160d0f1153c941
Daniel Braun <daniel@pomas.ca>
2022-09-12 11:03:27 -0400

    added livestream page

--- Shared/Entities/LiveStream.cs
+++ Shared/Entities/LiveStream.cs
@@ -5,18 +5,12 @@ namespace gbs.Shared.Entities;
 public class LiveStream
 {
     public int Id { get; set; }
-
-    [Column(TypeName = "timestamp without time zone")]
-    public DateTime Start { get; set; }
-
-    [Column(TypeName = "timestamp without time zone")]
-    public DateTime End { get; set; }
-
-    public string TimeZoneId { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
     public bool IsLive { get; set; } = false;
+    
+    public int GenerationId { get; set; }
+    public Generation Generation { get; set; } = null!;
 
     public List<Teacher> Teachers { get; set; } = new List<Teacher>();
-    public List<Generation> Generations { get; set; } = new List<Generation>();
 }
