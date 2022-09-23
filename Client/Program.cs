549bae7c7d7e827426be774f950745098eed6f2b
Daniel Braun <daniel@pomas.ca>
2022-09-22 21:27:47 -0400

    init mudblazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -16,12 +16,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using gbs.Client;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAuthService, AuthApiService>();
 builder.Services.AddScoped<IChurchService, ChurchService>();
--- Shared/Dtos/StreamCreateDto.cs
+++ Shared/Dtos/StreamCreateDto.cs
@@ -11,8 +11,9 @@ public class StreamCreateDto
     public string Url { get; set; } = string.Empty;
     public bool IsLive { get; set; } = false;
     
+    [Required]
     [Range(1, int.MaxValue, ErrorMessage = "Please select a generation")]
-    public int GenerationId { get; set; }
+    public int? GenerationId { get; set; }
 
     [Required, MinLength(1, ErrorMessage = "Please select at least one teacher")]
     public List<int> Teachers { get; set; } = new List<int>();
