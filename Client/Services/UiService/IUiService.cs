a07b48ca33d7ad993b36cd9445fc913aa2346355
Daniel Braun <daniel@pomas.ca>
2022-09-24 18:27:55 -0400

    Worked on theming

--- Client/Services/UiService/IUiService.cs
+++ Client/Services/UiService/IUiService.cs
@@ -4,9 +4,12 @@ namespace gbs.Client.Services.UiService;
 
 public interface IUiService
 {
+    public bool Loading { get; set; }
+    event Action LoadingChanged;
     List<AlertDto> Alerts { get; set; }
     event Action AlertsChanged;
-    Task ShowErrorAlert(string message, int statusCode);
+    Task ShowErrorAlert(string message, int statusCode = 400);
     void ShowSuccessAlert(string message);
-    void RemoveAlert(string alertId);
+    void LoadingStart();
+    void LoadingStop();
 }
