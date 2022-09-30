fc7ded791a3e90be7834d260b99b8320cfbf73a2
Daniel Braun <daniel@pomas.ca>
2022-09-30 15:33:54 -0400

    Fixes #8

--- Shared/Dtos/GenerationCreateDto.cs
+++ Shared/Dtos/GenerationCreateDto.cs
@@ -1,6 +1,9 @@
-﻿namespace gbs.Shared.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gbs.Shared.Dtos;
 
 public class GenerationCreateDto
 {
+    [Required, MinLength(3, ErrorMessage = "Name must be at least 3 characters long")]
     public string Name { get; set; } = string.Empty;
 }
