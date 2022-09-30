c333ac6140ad745e0c3e70b0e969fffef1adc391
Daniel Braun <daniel@pomas.ca>
2022-09-30 15:41:00 -0400

    Fixes #4

--- Shared/Dtos/RegisterDto.cs
+++ Shared/Dtos/RegisterDto.cs
@@ -4,14 +4,19 @@ namespace gbs.Shared.Dtos;
 
 public class RegisterDto
 {
+    [Display(Name = "First Name")]
     [Required, StringLength(100, MinimumLength = 3)]
     public string FirstName { get; set; } = string.Empty;
+
+    [Display(Name = "Last Name")]
     [Required, StringLength(100, MinimumLength = 3)]
     public string LastName { get; set; } = string.Empty;
-    [Required, EmailAddress]
-    public string Email { get; set; } = string.Empty;
+
+    [Required, EmailAddress] public string Email { get; set; } = string.Empty;
+
     [Required, StringLength(100, MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
+
     [Compare("Password", ErrorMessage = "The passwords do not match")]
     public string ConfirmPassword { get; set; } = string.Empty;
 }
--- Shared/Dtos/Students/StudentCreateDto.cs
+++ Shared/Dtos/Students/StudentCreateDto.cs
@@ -8,7 +8,9 @@ public class StudentCreateDto : IStudentCreateDto
     [Required, StringLength(100, MinimumLength = 3)]
     public string Name { get; set; } = string.Empty;
 
-    [Required] public DateTime? DateOfBirth { get; set; }
+    [Display(Name = "Date of Birth")]
+    [Required]
+    public DateTime? DateOfBirth { get; set; }
 
     [Required, StringLength(100, MinimumLength = 3)]
     public string Address { get; set; } = string.Empty;
@@ -16,16 +18,21 @@ public class StudentCreateDto : IStudentCreateDto
     [Required, StringLength(50, MinimumLength = 3)]
     public string City { get; set; } = string.Empty;
 
+    [Display(Name = "Province/State")]
     [Required, StringLength(50, MinimumLength = 3)]
     public string State { get; set; } = string.Empty;
 
     [Required, StringLength(50, MinimumLength = 3)]
     public string Country { get; set; } = string.Empty;
 
+    [Display(Name = "Postal Code")]
     [Required, StringLength(10, MinimumLength = 3)]
     public string PostalCode { get; set; } = string.Empty;
 
-    [Required] public MaritalStatus MaritalStatus { get; set; }
+    [Display(Name = "Marital Status")]
+    [Required]
+    public MaritalStatus MaritalStatus { get; set; }
+
     [StringLength(100, MinimumLength = 3)] public string Email { get; set; } = string.Empty;
     [StringLength(50, MinimumLength = 3)] public string Phone { get; set; } = string.Empty;
     public int? ChurchId { get; set; }
