30767b5a570eef60f81e86c7788821ee7f6fc2c3
Daniel Braun <daniel@pomas.ca>
2022-11-24 17:16:11 -0500

    add migrations for optional subject

---
+++ src/Infrastructure.Persistence/Migrations/20221124215414_OptionalSubjectInLesson.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gbs.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptionalSubjectInLesson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Subjects_SubjectId",
+                table: "Lessons");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "Lessons",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Subjects_SubjectId",
+                table: "Lessons",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Subjects_SubjectId",
+                table: "Lessons");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubjectId",
+                table: "Lessons",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Subjects_SubjectId",
+                table: "Lessons",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
