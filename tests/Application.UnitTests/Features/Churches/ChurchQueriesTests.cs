193d19f9024c1bd136c66549a1e7989e50406dca
Daniel Braun <daniel@pomas.ca>
2022-12-10 17:50:29 -0500

    added query test interface

--- tests/Application.UnitTests/Features/Churches/ChurchQueriesTests.cs
+++ tests/Application.UnitTests/Features/Churches/ChurchQueriesTests.cs
@@ -1,11 +1,12 @@
 ﻿using Gbs.Application.Features.Churches;
+using Gbs.Tests.Application.UnitTests.Common;
 
 namespace Gbs.Tests.Application.UnitTests.Features.Churches;
 
-public class ChurchQueriesTests : ChurchTestBase
+public class ChurchQueriesTests : ChurchTestBase, IQueryTests
 {   
     [Fact]
-    public async Task GetAll_ReturnsAllChurches()
+    public async Task GetAll_ReturnsAllRecords()
     {
         
         var churchQ = new ChurchQueries(Context, Mapper);
@@ -16,9 +17,24 @@ public class ChurchQueriesTests : ChurchTestBase
         Assert.NotNull(result.Data);
         Assert.Equal(3, result.Data.Count);
     }
-    
+
     [Fact]
-    public async Task GetById_ReturnsChurch()
+    public async Task GetAll_ReturnsEmptyList_WhenNoRecords()
+    {
+        // Arrange
+        var churchQ = new ChurchQueries(Context, Mapper);
+        Context.Churches.RemoveRange(Context.Churches);
+        await Context.SaveChangesAsync();
+        // Act
+        var result = await churchQ.GetAll();
+        // Assert
+        Assert.True(result.Success);
+        Assert.NotNull(result.Data);
+        Assert.Empty(result.Data);
+    }
+
+    [Fact]
+    public async Task GetById_ReturnsRecord()
     {
         var churchQ = new ChurchQueries(Context, Mapper);
         
@@ -27,5 +43,10 @@ public class ChurchQueriesTests : ChurchTestBase
         Assert.True(result.Success);
         Assert.NotNull(result.Data);
         Assert.Equal(1, result.Data.Id);
+    }
+
+    public async Task GetById_ReturnsNull_WhenNoRecord()
+    {
+        throw new NotImplementedException();
     }
 }
