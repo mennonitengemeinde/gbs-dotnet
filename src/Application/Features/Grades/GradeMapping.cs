8837484d167bf2e30f1e96345e26df9535286419
Daniel Braun <42745795+danielpomas@users.noreply.github.com>
2022-12-27 23:03:10 -0500

    added grade types form and list page

--- src/Application/Features/Grades/GradeMapping.cs
+++ src/Application/Features/Grades/GradeMapping.cs
@@ -7,7 +7,8 @@ public class GradeMapping : Profile
         CreateMap<Grade, GradeResponse>()
             .ForMember(dest => dest.StudentFirstName, opt => opt.MapFrom(src => src.Student.FirstName))
             .ForMember(dest => dest.StudentLastName, opt => opt.MapFrom(src => src.Student.LastName))
-            .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject.Name));
+            .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject.Name))
+            .ForMember(dest => dest.GradeType, opt => opt.MapFrom(src => src.GradeType.Name));
         
         CreateMap<CreateGradeRequest, Grade>()
             .ForMember(dest => dest.Date, opt => opt.MapFrom(src => DateOnly.FromDateTime(src.Date ?? DateTime.Now)))
