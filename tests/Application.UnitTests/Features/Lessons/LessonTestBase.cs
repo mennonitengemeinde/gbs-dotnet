c045b4664aca5a38fc86b609f7c0ba9f04973885
Daniel Braun <42745795+danielpomas@users.noreply.github.com>
2023-01-15 09:51:29 -0500

    finished status view in school page

--- tests/Application.UnitTests/Features/Lessons/LessonTestBase.cs
+++ tests/Application.UnitTests/Features/Lessons/LessonTestBase.cs
@@ -1,4 +1,6 @@
-﻿using Gbs.Application.Features.Lessons;
+﻿using Gbs.Application.Common.Interfaces.Services;
+using Gbs.Application.Features.Lessons;
+using Moq;
 
 namespace Gbs.Tests.Application.UnitTests.Features.Lessons;
 
@@ -7,7 +9,11 @@ public class LessonTestBase : GbsTestBase
     protected LessonTestBase()
     {
         Validator = new LessonValidator(Context);
+        var authedUserService = new Mock<IAuthenticatedUserService>();
+        authedUserService.Setup(x => x.GetUserId()).Returns("superAdmin");
+        AuthenticatedUserService = authedUserService.Object;
     }
 
     protected LessonValidator Validator { get; }
+    protected IAuthenticatedUserService AuthenticatedUserService { get; }
 }
