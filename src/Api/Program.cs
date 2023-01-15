d16831a08dd89461cff54562de47c4b02f7b9e8b
Daniel Braun <42745795+danielpomas@users.noreply.github.com>
2023-01-15 10:49:34 -0500

    updated to asp 7.0.2 and moved lesson state to lesson service

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -10,8 +10,6 @@ builder.Services.AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseResponseCompression();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,6 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseResponseCompression();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
