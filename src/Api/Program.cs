89057c3acd8fac7e275ebedbc0559cdaebe00156
Daniel Braun <42745795+danielpomas@users.noreply.github.com>
2023-09-17 00:11:17 -0400

    added serilog and updated tailwind

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,8 +1,10 @@
 using Gbs.Api;
 using Gbs.Application;
 using Gbs.Infrastructure.Persistence;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
@@ -22,6 +24,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
